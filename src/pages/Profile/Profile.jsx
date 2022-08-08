891e639e25acee9c2a80fc6a94d0472a39b836d5
mbarndouka <mmbarndouka@gmail.com>
2022-08-08 12:29:58 +0200

    #profile page one part done

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
+import './profile.css'
+import Logo from '../../assets/logo.svg'
+import Arrow from '../../assets/arrow-line.svg'
+
 function Profile() {
   return (
-    <div>
-      Profile
+    <div className='profile'>
+      <div className="profile-left">
+        <div className="profile-logo">
+          <img src={Logo} alt="" />
+        </div>
+      </div>
+      <div className='profile-right'>
+        <div className="back-btn">
+          <Link to='/signup'>
+        <img src={Arrow} alt="" />
+        </Link>
+        </div>
+        <div className="profile-right-wel">
+          <p>Welcome! Let&rsquo;s create your profile</p>
+        </div>
+        <div className="right-wel-btm">
+          <p>Let&rsquo;s others get to know you better! you can do these later</p>
+        </div>
+        <form className='formul'>
+        <div className="right-avatar">
+          <p>Add an Avatar</p>
+        </div>
+        <div className="right-img-avatar">
+          <div className="left-picture-avatar">
+            <img src="" alt="yourimage" />
+          </div>
+          <div className="right-picture-choose">
+            <button className="choose" type=''>
+              Choose Avatar
+            </button>
+            <button className='choosen' type=''> choose one our default</button>
+          </div>
+        </div>
+        <div className="right-add-community">
+          <p>Add your community</p>
+        </div>
+        <div className="right-add-groupe">
+          <input type="text" placeholder='Enter the community you&rsquo;re joining' />
+        </div>
+        <div className='save'>
+          <button className='saver' type='submit'>Save</button>
+        </div>
+        </form>
+      </div>
     </div>
   )
 }
