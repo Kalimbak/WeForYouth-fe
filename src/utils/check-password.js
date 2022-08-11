c175034691596320615b7d6242b423212c7d9597
react.solvit <react.solvit@gmail.com>
2022-08-11 11:04:27 +0200

    integrated

---
+++ src/utils/check-password.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @param {string} password 
+ * @param {string} confirmPassword 
+ * @returns {boolean}
+ */
+export default function checkPassword(password, confirmPassword){
+    return password === confirmPassword;
+}
